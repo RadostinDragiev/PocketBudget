7c46bfec0b1346cb8411263b582799c114d08fe0
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2023-06-20 13:47:53 +0300

    Small changes on RegisterUserBindingModel and added Spring Security logging

--- src/main/java/com/pocketbudget/model/binding/RegisterUserBindingModel.java
+++ src/main/java/com/pocketbudget/model/binding/RegisterUserBindingModel.java
@@ -1,5 +1,6 @@
 package com.pocketbudget.model.binding;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class RegisterUserBindingModel {
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String email;
     private String firstName;
