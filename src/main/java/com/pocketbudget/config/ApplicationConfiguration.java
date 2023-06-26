2e951f4175ed0508298ebf6f6338b49e7a4a521d
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2023-06-27 01:54:51 +0300

    Minor changes

--- src/main/java/com/pocketbudget/config/ApplicationConfiguration.java
+++ src/main/java/com/pocketbudget/config/ApplicationConfiguration.java
@@ -1,7 +1,9 @@
 package com.pocketbudget.config;
 
 import com.pocketbudget.util.DateTimeApplier;
+import com.pocketbudget.util.EmailManager;
 import com.pocketbudget.util.impl.DateTimeApplierImpl;
+import com.pocketbudget.util.impl.EmailManagerImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,5 +25,10 @@ public class ApplicationConfiguration {
     @Bean
     public DateTimeApplier dateTimeApplier() {
         return new DateTimeApplierImpl();
+    }
+
+    @Bean
+    public EmailManager emailManager() {
+        return new EmailManagerImpl();
     }
 }
